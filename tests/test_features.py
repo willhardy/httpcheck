3c4a66d8c6fddce28496d807e62fe96b2c25c5e6
Will Hardy <will@willhardy.com.au>
2020-06-09 01:28:43 +0200

    Remove test that would run event loop

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,5 +1,3 @@
-import datetime
-
 import httpcore
 import pytest
 import pytest_httpx
@@ -51,37 +49,3 @@ async def test_identifier(monitor, httpx_mock):
     await monitor.make_attempt()
     (request1,) = httpx_mock.get_requests()
     assert request1.headers["user-agent"] == "httpcheck/hello"
-
-
-@pytest.mark.skip("timeout?")
-@pytest.mark.asyncio
-async def test_frequency(monitor, httpx_mock):
-    # Alternatve online and offline
-    status_codes = [200, 500, 200, 500, 500]
-
-    def alternating_status_code(*args, **kwargs):
-        sc = status_codes.pop(0) if status_codes else 200
-        return pytest_httpx.to_response(status_code=sc)
-
-    httpx_mock.add_callback(alternating_status_code)
-
-    monitor.frequency_online = 20
-    monitor.frequency_offline = 3
-    logs = monitor.monitor()
-    get_dt = lambda val: datetime.datetime.fromisoformat(val.timestamp)
-
-    # Create a list of the seconds between each attempt
-    deltas = []
-
-    # async generator means old school looping
-    t0 = get_dt(await logs.__anext__())
-    i = 0
-    async for log in logs:
-        if i >= 5:
-            break
-        ti = get_dt(log)
-        deltas.append(int((ti - t0).total_seconds()))
-        t0 = ti
-        i += 1
-
-    assert deltas == [20, 3, 20, 3, 3]
